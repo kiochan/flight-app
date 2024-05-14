2059882640bd4b371a7639364217c9d78bd51e2d
祈緒ちゃん - Kiochan <sunxingchen@live.com>
2024-05-14 11:51:52 +0200

    add button with directive

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,13 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TabComponent } from '../shared/controls/tab/tab.component';
 import { TabbedPaneComponent } from '../shared/controls/tabbed-pane/tabbed-pane.component';
+import { ClickWithWarningDirective } from '../shared/controls/click-with-warning.directive';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-about',
   standalone: true,
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css'],
-  imports: [CommonModule, TabComponent, TabbedPaneComponent],
+  imports: [
+    CommonModule,
+    TabComponent,
+    TabbedPaneComponent,
+    ClickWithWarningDirective,
+  ],
 })
-export class AboutComponent {}
+export class AboutComponent {
+  router = inject(Router);
+
+  warn(): void {
+    this.router.navigate(['/']);
+  }
+}
